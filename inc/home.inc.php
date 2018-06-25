e67206cfce9859f4298a5a9aeeedb87d5f8e2e3c
Jamie Gregory <jamiegregory.icloud@me.com>
2018-06-25 21:33:29 +0100

    Add basic styling; Display long URL on web front end

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -1,14 +1,25 @@
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="public/css/home.css"/>
     </head>
 
     <body>
-        <form method="post">
-            <input type="text" name="url" />
-            <input type="submit" />
-        </form>
-        <p>
-            <?= $squished_url ?? ""; ?>
-        </p>
+        <div class="container">
+            <form method="post" class="squish-input">
+                <div class="logo"></div>
+                <div class="inline-fix">
+                    <input type="text" name="url" />
+                    <input type="submit" />
+                </div>
+                <p>
+                    <?php
+                    // Only need to check if one value is set
+                    if(isset($squished_url)) {
+                        echo "<small>{$long_url}</small> <a href='{$squished_url}'>{$squished_url}</a>";
+                    }
+                    ?>
+                </p>
+            </form>
+        </div>
     </body>
 </html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    
+
     require(__DIR__ . "/lib/Squisher.class.php");
 
     $squisher = null;
@@ -14,7 +14,10 @@
         // Do the stuff to squish & add the URL to the DB
         $squisher = new Squisher();
         $squisher->squish($url);
+
+        // Make the long and squished URL available as vars to pickup in the template.
         $squished_url = $squisher->get_squished_url();
+        $long_url = $squisher->get_long_url();
     }
 
     include(__DIR__ . "/inc/home.inc.php");
